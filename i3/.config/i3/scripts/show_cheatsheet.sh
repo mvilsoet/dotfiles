6916027b05bd1e648c41af4c15d446154c0602db
Michael Vilsoet <57650580+mvilsoet@users.noreply.github.com>
2025-06-29 21:04:37 -0400

    Update show_cheatsheet.sh

--- i3/.config/i3/scripts/show_cheatsheet.sh
+++ i3/.config/i3/scripts/show_cheatsheet.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rofi -e "$(column -t -s $'\t' ~/.config/i3/scripts/cheatsheet.txt)"
+column -t -s $'\t' ~/.config/i3/cheatsheet.txt | rofi -dmenu -p "i3wm Cheatsheet" -lines 15 -columns 2 -width 80 -hide-scrollbar
